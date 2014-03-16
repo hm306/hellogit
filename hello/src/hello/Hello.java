382281504c158ca0d66e12900e33f2dc42cdfcb5
Hiromu <Hiromu@192.168.11.2>
2014-03-16 13:26:49 +0900

    文言修正

--- hello/src/hello/Hello.java
+++ hello/src/hello/Hello.java
@@ -3,5 +3,6 @@ package hello;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Java,via Eclipse on Windows!");
+		System.out.println("ƒwƒ[JavaB");
 	}
 }
